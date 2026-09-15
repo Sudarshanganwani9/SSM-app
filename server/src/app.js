1df7453c56612a313f1438d1ccaf21747f4519f6
Sudarshan Ganwani <sidkushwah9@gmail.com>
2026-09-15 16:16:30 +0530

    trust proxy fix

--- server/src/app.js
+++ server/src/app.js
@@ -20,6 +20,8 @@ const auditRoutes = require('./routes/auditRoutes');
 
 const app = express();
 
+app.set('trust proxy', 1);
+
 app.use(
   cors({
     origin: process.env.CLIENT_URL || 'http://localhost:5173',
